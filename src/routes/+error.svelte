f1f457e05e35870592da82e049b323c3b3541b80
yuiyamu <yuiyamuki@gmail.com>
2025-07-20 10:19:34 -0500

    redid 404 page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,27 +1,51 @@
 
 <svelte:head>
-    <title>sydnmc | 404</title>
+    <title>yurukyan | {page.status}</title>
 </svelte:head>
 
 <script lang="ts">
     import { page } from '$app/state';
+    import { PUBLIC_HOME_LINK } from '$env/static/public';
 </script>
 
-<div class="header-wrapper">
-    <a href="https://syd.yuru.ca">
-        <img id="image" src="https://api.yuru.ca/images/sydneypfp.png" alt="sydney's shima rin pfp">
+<div id="page">
+    <a id="header" href={PUBLIC_HOME_LINK}>
+        <img alt="system pfp" src="/common/systempfp.jpg">
+        <h1 id="title">{page.status}△</h1>
     </a>
-    <h1 id="title">{page.status} | sydnmc</h1>
-    <span><strong>- english -</strong></span>
-    <br>
-    <span>sorry, the page you're looking for does not exist.<br><a href="https://syd.yuru.ca">click here</a> to go back to the site homepage.</span>
-    <br><br>
-    <span><strong>- 日本語 -</strong></span><br><span>ごめんなさいが、探してるのページは存在ではありません。<br>ホームページに戻るには、<a href="https://syd.yuru.ca">ここ</a>をクリックしてください。</span>
+    <strong>- english -</strong>
+    <p>
+        {#if page.status === 404}
+            sorry, the page you're looking for does not exist.
+        {:else if page.status === 500}
+            sorry, the server ran into an internal error.
+        {:else}
+            sorry, an unknown error occured.
+        {/if}
+        <br>
+        <a href="https://syd.yuru.ca">click here</a> to go back to the site homepage.
+    </p>
+    <div style="display: flex; justify-content: center;">
+        <div id="divider"></div>
+    </div>
+    <strong>- 日本語 -</strong>
+    <p>
+        {#if page.status === 404}
+            ごめんなさいが、探してるのページは存在ではありません。
+        {:else if page.status === 500}
+            ごめんなさいが、サーバーで内部エラーが発生しました。
+        {:else}
+            ごめんなさいが、不明なエラーが発生しました。
+        {/if}
+        <br>
+        ホームページに戻るには、<a href="https://syd.yuru.ca">ここ</a>をクリックしてください。
+    </p>
 </div>
 
 <style>
-.header-wrapper {
-    padding: 15px 15px;
+#page {
+    position: relative;
+    padding: 30px 30px;
     text-align: center;
     background-color: var(--sydney-main);
     border-radius: 10px;
@@ -30,20 +54,46 @@
     margin-top: 35px;
 }
 
-#image {
+#header {
+    display: flex;
+    text-align: left;
+    align-items: center;
+    text-decoration: none;
+}
+
+
+img {
     border-radius: 50%;
-    width: 10%;
+    width: 100px;
 }
 
 h1 {
+    display: inline;
     font-family: Kyokasho, sans-serif;
     font-weight: normal;
     color: var(--offwhite);
-    font-size: 58px;
-    margin-top: 10px;
+    font-size: 70px;
+    margin-left: 10px;
+    margin-top: 0;
+    margin-bottom: 0;
 }
 
-span {
+strong {
     color: var(--offwhite);
+} p {
+    color: var(--offwhite);
+    margin-top: 0;
+}
+
+a {
+    color: var(--text);
+}
+
+#divider {
+    height: 3px;
+    width: 50%;
+    background: linear-gradient(90deg,rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0.4) 15%, rgba(255, 255, 255, 0.4) 85%, rgba(255, 255, 255, 0) 100%);
+    margin-top: 15px;
+    margin-bottom: 15px;
 }
 </style>
