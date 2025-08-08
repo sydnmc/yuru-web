93a2c3f0f26618931c736ff3fcd5225f45224e3d
yuiyamu <yuiyamuki@gmail.com>
2025-08-07 19:52:10 -0500

    added my about me page to localization file

--- apps/yuru-lilac/src/routes/whoami/+page.svelte
+++ apps/yuru-lilac/src/routes/whoami/+page.svelte
@@ -7,61 +7,58 @@
 <script lang="ts">
     import { Header } from "@repo/yuru-assets";
     import "@repo/yuru-static/assets/base.css";
+    import { _ } from "@repo/i18n";
 </script>
 
 <Header person="lilac" page="whoami"/>
 <div id="background"></div>
 <section id="page-content">
-    <h2>ꕤ who am i, compared to sydney?</h2>
+    <h2>{$_('lilac.whoAmI.title')}</h2>
     <div class="horizontal-divide"></div>
-    <p>if you’re already familiar with DID - you’ll probably understand right away who i am, and what my relationship with sydney is like. i’m one of her alters, but front quite a bit and have a large presence in her life. if terms like “front” or “alter” are unfamiliar to you, then don’t worry~! you’ll soon have a much better understanding of what DID is, and importantly to us, how it shows up in our lives. for the completely uninitiated, here’s a simple overview from wikipedia that deals more with the direct consequences in one’s life. (i won’t get into the complicated history or controversies around the disorder, that’s not for here~)</p>
+    <p>{$_('lilac.whoAmI.intro')}</p>
     <div id="did-definition">
-        <img id="sya-my-beloved" src="https://media1.tenor.com/m/B8PaZ6UGHwYAAAAd/aurora-somnia-goodereste-princess-syalis.gif" alt="sya trying to understand DID">
-        <p><strong>dissociative identity disorder (DID)</strong> is characterized by the presence of at least <strong>two distinct</strong> and <strong>relatively enduring</strong> personality states. the disorder is accompanied by <strong>memory gaps</strong> more severe than could be explained by ordinary forgetfulness.</p>
+        <img id="sya-my-beloved" src="https://media1.tenor.com/m/B8PaZ6UGHwYAAAAd/aurora-somnia-goodereste-princess-syalis.gif" alt={$_('lilac.whoAmI.syaAltText')}>
+        <p>{@html $_('lilac.whoAmI.didDefinition')}</p>
     </div>
-    <p>this definition applies quite well to how i experience life - anyone who knows us well will clearly see at least two (four, in fact) personality states, and inexplicable memory gaps between these personality states. however, like any disorder, it quickly gets messy and varies a lot from person to person.</p>
-    <p><strong>so now, i'll explain in my own words~!</strong></p>
-    <p>in july 2024, something happened that made me go into a sort of “safe mode” of myself - i couldn’t really bring myself to interact with the usual people that i knew, and felt a bit uncomfortable identifying myself as sydney. this continued on for a few months, on and off - but i didn’t really think anything of it at the time. then, i really just considered it to be "how i felt" whenever i needed some distance from "my" main accounts. after a few months of this, i started to notice that i couldn't quite remember what happened when i was on this other account, and presumably vice versa (though i can’t confirm this, i’m not sydney :p). at some point, for reasons that i can’t quite pinpoint (but now seem obvious in hindsight), i picked out a name for myself - lilac. lilac just had a ring to it that felt… right, for lack of better words. i still wasn’t convinced at this point that i was any different from sydney despite, well… identifying myself not as sydney. </p>
+    <p>{$_('lilac.whoAmI.didDefinitionExtra')}</p>
+    <p><strong>{$_('lilac.whoAmI.body0Intro')}</strong></p>
+    <p>{$_('lilac.whoAmI.body0')} </p>
     <div id="computer-side-img">
         <div id="left-computer-content">
-                <p>unbeknownst to my doubtful self, there were yet more differences between sydney and i that some of our friends could spot. not only do i mostly not remember what sydney does, but i've also been told i talk differently than her, both through text and my tone of voice irl. for all of those reasons, it eventually clicked in my head that i may not be different than her - which was quite the shocking thing to realize. with enough evidence of <i>something</i> going on in our head, i talked to a psychologist at my university about my whole situation, and sure enough, in october 2024, we were diagnosed with DID.</p>
-                <p>from this point on, i could no longer consider it a “bit” or whatever else i may have used to justify it to myself or my friends - i’m genuinely quite different from sydney, and now consider us to essentially be separate people. i wouldn’t say we’re completely different, as there are still a lot of similarities between us naturally. those who either study DID or have it themselves would call sydney the <strong>host</strong> of our system, and me an <strong>alter</strong> of her. i’ll sometimes use these terms occasionally, but i think it’s simplest to just consider us to be separate people.</p>
-                <p>i’ve had to make a lot of personal strides to gain my independence from sydney, mostly just to be comfortable having my own little spot on the internet and be surrounded by friends who truly appreciate me for being lilac~ i now have my own socials on most platforms that i use whenever i <strong>front</strong> (essentially whenever i wake up and sydney goes to sleep), and even my own laptop. i’m so much happier to have these separations from her, and now feel very comfortable and confident in the fact that i am, undeniably, lilac.</p>
+                <p>{$_('lilac.whoAmI.body1')}</p>
+                <p>f{$_('lilac.whoAmI.body2')}</p>
+                <p>{$_('lilac.whoAmI.body3')}</p>
         </div>
         <div id="computer-side-img-container">
-                <img src="/computers.jpg" alt="our individual computers - a thinkpad and acer swift">
-                <span>our computers, side by side~ mine is the thinkpad, sydney's is the acer swift :3</span>
+                <img src="/computers.jpg" alt={$_('lilac.whoAmI.computersAltText')}>
+                <span>{$_('lilac.whoAmI.photoCaption')}</span>
         </div>
     </div>
-    <h2>ꕤ qna</h2>
+    <h2>{$_('lilac.whoAmI.faqSection')}</h2>
     <div class="horizontal-divide"></div>
-    <p>since i’ve gotten a lot of questions about the intricacies of how our system works (especially after making it clear that we share an osu! account when i changed our username there), i thought i’d answer a few questions that i see pop up again and again.</p>
+    <p>{$_('lilac.whoAmI.faqIntro')}</p>
     <div class="qna-question">
-        <h3>q. what caused this to happen, and what does it do for you?</h3>
-        <p><strong>a.</strong> when i initially wrote the first version of this page, i had made it sound like this was something that seemingly occurred without any real reason - simply from me feeling “different” from sydney and wanting to go by a different name. while this is partially true, it’s not exactly the full story. simply put, this split between us occurred as a result of trauma. what exactly this was is something that i do not at all feel comfortable disclosing, and something that i don’t exactly remember - which is partially why DID occurs in people. it’s a way of compartmentalizing trauma, having alters that take over for someone that don’t have memories of said trauma in order to protect someone. from the sessions with my psychologist i had leading up to our diagnosis, i’ve gathered that, essentially,  i exist to protect sydney when she’s feeling overwhelmed, with less capacity for social interactions but more tolerance for the sources of her stress. DID is just one of the ways that the mind can process trauma and heal from it, but it’s the method that all of the complicated chemicals in our brain decided to go about doing so, which is why i’m here~</p>
+        <h3>{$_('lilac.whoAmI.faqQuestion0')}</h3>
+        <p><strong>a.</strong> {$_('lilac.whoAmI.faqAnswer0')}</p>
     </div>
     <div class="qna-question">
-        <h3>q. you mentioned another alter earlier, what’s that about?</h3>
-        <p><strong>a.</strong> recently, i’ve seen another alter of ours (kyatarii / hazel), who seems to be appearing both as a result of a secondary traumatic event that happened to us, as well as a tendency for me to be fronting for very long periods of time recently and… simply needing a bit of rest from time to time. you could think of her as also being a “safe mode” for me, another layer of redundancy to keep us functioning in some sort of way, even if it’s reduced~</p>
+        <h3>{$_('lilac.whoAmI.faqQuestion1')}</h3>
+        <p><strong>a.</strong> {$_('lilac.whoAmI.faqAnswer1')}</p>
     </div>
     <div class="qna-question">
-        <h3>q. do you have any proof of your diagnosis?</h3>
-        <p><strong>a.</strong> i’m honestly a little shocked that i’ve gotten this question before, and multiple times at that. the answer is yes, but not any that i would be okay sharing with anyone. i’m already sharing a bit more than i’m comfortable with on the internet in order to explain my existence to those who are genuinely curious - but this is much too far for me. i personally feel that these types of questions come out of a place of malice rather than genuine curiosity, but i’d just like to answer this in case anyone may ask again in the future.</p>
+        <h3>{$_('lilac.whoAmI.faqQuestion2')}</h3>
+        <p><strong>a.</strong> {$_('lilac.whoAmI.faqAnswer2')}</p>
     </div>
     <div class="qna-question">
-    <h3>q. how do relationships (both platonic and romantic) function with DID?</h3>
-        <p><strong>a.</strong> this is a really interesting question, because we found ourselves in a unique situation regarding relationships~ in terms of friendships, i share a lot of friends with sydney. when i was first fronting, i felt very lonely because isolating myself felt like the only sensible option at the time… but i eventually invited myself into sydney’s discord server, and got to know all of the people she had in there. at the time, i acted as a separate person and didn’t make my relationship to sydney apparent. but, over time, i grew more comfortable with people knowing the connection between us, and was able to find a friend group that accepted me for being lilac. i also now know a few people that have never talked to sydney, which honestly makes me really comfortable~ (⸝⸝&gt; ω &lt;⸝⸝) i really do value my independence, so i’m happy to have found friends who value that.
-        <br><br>as for romantic relationships, things get a little more complicated. a little after i started fronting regularly and recognizing my independence, i found someone who i dated for a number of months - as lilac, and not as anyone else. this was a little complicated, as i would regularly sleep for days at a time… and eventually led to us ending things. when sydney starting dating someone after this, i had the same mindset as when i was in my own relationship, and left her to her own devices. however, when sydney started to front less and less around the start of 2025, i started getting closer and closer to her girlfriend, and… we eventually decided that it would be best if we were also together. as of now, the three of us are all dating someone quite wonderful who understands all of us very well, and i couldn’t be happier with her. (*ᴗ͈ˬᴗ͈)ꕤ*.ﾟ</p>
+    <h3>{$_('lilac.whoAmI.faqQuestion3')}</h3>
+        <p><strong>a.</strong> {$_('lilac.whoAmI.faqAnswer3')}
+        <br><br>{$_('lilac.whoAmI.faqAnswer3_1')}</p>
     </div>
-    <h2>ꕤ no but really, who am <i>i</i> ?</h2>
+    <h2>{@html $_('lilac.whoAmI.reallySection')}</h2>
     <div class="horizontal-divide"></div>
-    <p>hi! if you don't know me or sydney at all, then nice to meet you c: i'm a 19 year old girl living in toronto, on, &#127464;&#127462;. i go to york university and am studying computer science, as you may have been able to guess...
-            i started taking a class in web development recently, and thought it would be cool if i made my own personal site as i'd seen some of my friends do the same - so that's why this is here!!
-    </p>
-    <p>i make charts (called maps) for a rhythm game called osu!, which is what the button in the header bar shows (the content that i've made personally). sydney is known a lot better than me for this, but i also do stuff here and there (and stuff with her, as well :O)
-        sydney also makes her own music occasionally, which she goes into more detail about on her page.
-    </p>
-    <p>since i pretty much exist as a proxy of sydney, there's not a whole lot i can exactly claim to be "mine", other than the few things that i've done so far. however, i hope this was still informative to anyone who happens to stumble across this, or is linked to it by me! thank you c:</p>
+    <p>{$_('lilac.whoAmI.reallyBody0')}</p>
+    <p>{$_('lilac.whoAmI.reallyBody1')}</p>
+    <p>{$_('lilac.whoAmI.reallyBody2')}</p>
 </section>
 
 <style>
